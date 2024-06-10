1816f7897ac87be946fcd8853815789648305b19
Aditya Syam <97302238+adityasyam@users.noreply.github.com>
2024-06-09 21:45:30 -0400

    checking comments

--- constants.py
+++ constants.py
@@ -5,6 +5,16 @@ This file contains all the constants used in the free cash flows valuation.
 Edit the constants below as required to suit you specific requirements and the 
 code base will adapt automatically to give you your output.
 """
+number_of_years = 5 #this is the number of years forecasted
 
-market_risk_premium = 2 #this is the market risk premium, expressed in %
+industry_beta = 1.17 
+"""
+this is the industry beta of the given company you want to evaluate; to find
+industry beta values check the link 
+"""
+
+market_risk_premium = 6 #this is the market risk premium, expressed in %
+
+terminal_growth_rate = 2 
+
 
