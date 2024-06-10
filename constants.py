f0d0da11b66d42c3e12cde4f5c3f5b5f20e368c0
Aditya Syam <97302238+adityasyam@users.noreply.github.com>
2024-06-09 20:24:07 -0400

    created constants file

---
+++ constants.py
@@ -0,0 +1,10 @@
+"""
+TO BE MODIFIED AS REQUIRED BY USER.
+
+This file contains all the constants used in the free cash flows valuation. 
+Edit the constants below as required to suit you specific requirements and the 
+code base will adapt automatically to give you your output.
+"""
+
+market_risk_premium = 2 #this is the market risk premium, expressed in %
+
