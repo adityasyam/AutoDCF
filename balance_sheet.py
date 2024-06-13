485791bf8af75dd73ce7ec7edd4cc52c1834f7aa
Aditya Syam <97302238+adityasyam@users.noreply.github.com>
2024-06-13 14:19:29 -0400

    basic imports for 2 statements

---
+++ balance_sheet.py
@@ -0,0 +1,5 @@
+from constants import number_of_years, tax_rate
+
+from income_statement import forecast_revenues_base, forecast_net_income_base
+
+import numpy as np
---
+++ cash_flow_statement.py
@@ -0,0 +1,3 @@
+from constants import number_of_years
+
+import numpy as np
